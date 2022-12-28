c53c74e5a2c43eb4a50f56564f272a8ea0ebf676
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-12-28 14:13:34 +0100

    feat: chained routes and setup basic task controllers for CRUD

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,7 +1,23 @@
 const getAllTasks = (req, res) =>{
-    res.send("All Tasks")
+    res.send("Get all Tasks")
+}
+const createTask = (req, res) =>{
+    res.json(req.body)
+}
+const getTask = (req, res) =>{
+    res.json({id : req.params.id})
+}
+const updateTask = (req, res) =>{
+    res.send("Update Tasks")
+}
+const deleteTask = (req, res) =>{
+    res.send("Delete Tasks")
 }
 
 module.exports = {
     getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const {getAllTasks} = require('../controllers/tasks')
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
-router.get('/', getAllTasks)
+router.route("/").get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
-module.exports = router
+module.exports = router;
