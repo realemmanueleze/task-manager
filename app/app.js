64f87c71f18f51b81103c4dbff67b3b86d0bc5a2
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-12-28 12:16:35 +0100

    feat: setting up task routes and json middleware

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+    return
+})
+
+app.use('/api/v1/tasks', tasks)
+
+app.listen(3000, ()=>{
+    console.log('Server listening on port 3000')
+})
