dce53b3c1d4272f0a952c5925cfebe2a1ab8674f
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-12-31 16:27:33 +0100

    feat: added validation to task schema

--- app/models/tasks.js
+++ app/models/tasks.js
@@ -1,8 +1,16 @@
 const { Schema, model } = require("mongoose");
 
 const TaskSchema = new Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide a name"],
+    trim: true,
+    maxlength: [20, "name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = model("Tasks", TaskSchema)
+module.exports = model("Tasks", TaskSchema);
