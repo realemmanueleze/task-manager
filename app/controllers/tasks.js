bb73928c24253c8e1527cbf0806d734bb7d890b5
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-12-31 16:03:24 +0100

    feat: connceted task model to createTask controller

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,8 +1,12 @@
+const Task = require('../models/tasks')
+
+
 const getAllTasks = (req, res) =>{
     res.send("Get all Tasks")
 }
-const createTask = (req, res) =>{
-    res.json(req.body)
+const createTask = async (req, res) =>{
+   const task = await Task.create(req.body)
+   res.status(201).json(task)
 }
 const getTask = (req, res) =>{
     res.json({id : req.params.id})
