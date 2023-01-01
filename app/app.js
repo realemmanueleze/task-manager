7b7a72e64d83f798221dec3fcdba89b9dd820c68
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-01-01 10:08:04 +0100

    feat: added Route not found middleware

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middlewares/not-found')
 
 // middleware
 app.use(express.static('./public'))
 app.use(express.json())
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
 const start = async ()=>{
     try {
